6cc678e8be40f0659a7ea2e7d1d5519130fdc2f0
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-04-13 17:50:04 -0600

    update queries to return results

--- queries/index.tsx
+++ queries/index.tsx
@@ -1,6 +1,6 @@
 import { parse, evaluate } from "groq-js";
 import groq from "groq";
-import dataset from "@/sample-data/brand-category-products.json";
+// import dataset from "@/sample-data/brand-category-products.json";
 import brands from "@/sample-data/brand.json";
 import categories from "@/sample-data/category.json";
 
@@ -8,21 +8,60 @@ export async function groqQuery(query: string) {
   // Returns an ESTree-inspired syntax tree
   const tree = parse(query);
 
-  const ex = [...brands, ...categories];
+  const dataset = [...brands, ...categories];
   // Evaluate a tree against a dataset
-  const value = await evaluate(tree, { dataset: ex });
+  const value = await evaluate(tree, { dataset });
 
   // Gather everything into one JavaScript object
   let result = await value.get();
 
+  console.log("\n\n ===== Result ===== \n\n");
   console.dir(result, { depth: null });
+  console.log("\n\n ===== End Result ===== \n\n");
   return result;
 }
 
 export async function getBrands() {
-  const query = await groq`*[_type == "brand"] {
+  const query = groq`*[_type == "brand"]`;
+  const result = await groqQuery(query);
+  return result;
+}
+
+export async function getCategories() {
+  const query = groq`*[_type == "category"]`;
+  const result = await groqQuery(query);
+  return result;
+}
+
+/**
+ * Example query of flattening data.
+ * @example
+ * [
+ *  {
+ *    name: 'Patagonia',
+ *    'womens-jackets': [
+ *      "Women's Better Sweater Fleece Jacket",
+ *      "Women's Nano Puff Jacket",
+ *      'Greenland Winter Jacket Women',
+ *      'Keb Eco-Shell Jacket Women'
+ *    ]
+ *  },
+ *  {
+ *    name: 'Fjallraven',
+ *    'womens-jackets': [
+ *      "Women's Better Sweater Fleece Jacket",
+ *      "Women's Nano Puff Jacket",
+ *      'Greenland Winter Jacket Women',
+ *      'Keb Eco-Shell Jacket Women'
+ *    ]
+ *  }
+ * ]
+ */
+export async function fetchWomensJacketsByBrand() {
+  const query = groq`*[_type == "brand"] {
     name,
-    categories[]->,
+    "womens-jackets": *[_type == "category" && slug == "womens-jackets"].products[].name,
   }`;
   const result = await groqQuery(query);
+  return result;
 }
