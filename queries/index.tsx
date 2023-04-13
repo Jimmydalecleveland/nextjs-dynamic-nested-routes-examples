1901826560bfa39a665487a6a0348a5053d223b8
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-04-13 16:15:13 -0600

    add separate brand and category data shapes for groq
    This allows us to query for specific types, and
    to find the relations between those types.
    
    Groq seems to require specific keywords to figure this out. So each category
    must have a `_id` field that matches the `_ref` field of the type
    referring to it. It also requires `_type` of `reference`, I believe.

--- queries/index.tsx
+++ queries/index.tsx
@@ -1,28 +1,28 @@
 import { parse, evaluate } from "groq-js";
 import groq from "groq";
 import dataset from "@/sample-data/brand-category-products.json";
+import brands from "@/sample-data/brand.json";
+import categories from "@/sample-data/category.json";
 
 export async function groqQuery(query: string) {
   // Returns an ESTree-inspired syntax tree
   const tree = parse(query);
 
-  const ex = [
-    { _type: "brand", name: "Michael" },
-    { _type: "category", name: "Bluth Company" },
-  ];
+  const ex = [...brands, ...categories];
   // Evaluate a tree against a dataset
-  const value = await evaluate(tree, { dataset });
+  const value = await evaluate(tree, { dataset: ex });
 
   // Gather everything into one JavaScript object
   let result = await value.get();
 
-  console.log(result);
+  console.dir(result, { depth: null });
   return result;
 }
 
 export async function getBrands() {
-  const query = await groq`*[ _type == "brand" ] {
+  const query = await groq`*[_type == "brand"] {
     name,
+    categories[]->,
   }`;
   const result = await groqQuery(query);
 }
